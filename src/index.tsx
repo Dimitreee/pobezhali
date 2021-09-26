cd4bc3c362c3d058def1dd6359d556372cba3dbe
Dimitreee <danewdima@gmail.com>
2021-09-26 09:09:05 +0300

    update index.tsx, update Readme.md

--- src/index.tsx
+++ src/index.tsx
@@ -10,22 +10,31 @@ import { ModalContextProvider } from './components/Modal/ModalContext'
 import { router } from './routes'
 
 
-bridge.send("VKWebAppInit");
+bridge.send('VKWebAppInit');
 
-router.start()
 
-ReactDOM.render(
-    <ReduxProvider store={store}>
-        <RouterContext.Provider value={router}>
-            <MapProvider>
-                <ModalContextProvider>
-                    <App />
-                </ModalContextProvider>
-            </MapProvider>
-        </RouterContext.Provider>
-    </ReduxProvider>,
-    document.getElementById("root")
-);
+bridge.subscribe(({ detail: { type, data }}) => {
+    if (type === 'VKWebAppInitResult') {
+        //@ts-ignore
+        if (data?.error_type !== 'auth_error' && data?.result === true) {
+            router.start()
+
+            ReactDOM.render(
+                <ReduxProvider store={store}>
+                    <RouterContext.Provider value={router}>
+                        <MapProvider>
+                            <ModalContextProvider>
+                                <App />
+                            </ModalContextProvider>
+                        </MapProvider>
+                    </RouterContext.Provider>
+                </ReduxProvider>,
+                document.getElementById("root")
+            );
+        }
+    }
+});
+
 
 if (process.env.NODE_ENV === "development") {
     import("./eruda").then(({ default: eruda }) => {}); //runtime download
